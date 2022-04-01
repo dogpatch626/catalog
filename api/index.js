9723fb98f0db81d437b37077a5958ba2e9c9b98f
dogpatch626 <djpepin1299@gmail.com>
2022-04-01 19:03:26 -0400

    connecting to mongo db

--- api/index.js
+++ api/index.js
@@ -2,8 +2,25 @@
 //and mongodb
 
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const routes = require("./routes/router");
+const cors = require("cors");
+const  startServer = async () =>{
+app.use(express.json());
+app.use(cors()); 
+
+
+await mongoose 
+  .connect(process.env.MONGO_CONNECTION)
+  .then(()=>{
+    console.log("Mongo Connected"); 
+  })
+  .catch((err) => {
+    console.log("something went wrong : ", err); 
+  })
 
 app.route("/clothing")
 .get(routes);
@@ -11,3 +28,8 @@ app.route("/clothing")
 app.listen(4000, () => {
   console.log("app is listening on port 4000");
 });
+
+}
+
+
+startServer(); 
